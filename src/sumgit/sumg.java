3cdb301c0b67b2b0a6619e66a5dc36e3451a020d
Pasupuleti Venkatesh <pasupuletivenkatesh1900@gmail.com>
2022-05-01 15:36:59 +0530

    adding sumg project

---
+++ src/sumgit/sumg.java
@@ -0,0 +1,13 @@
+package sumgit;
+
+public class sumg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =5;
+		int b =10;
+		System.out.println("sum of : "+ a+b);
+
+	}
+
+}
