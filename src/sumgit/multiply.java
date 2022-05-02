d047c9aba93d4132fffcb7924c9509f7eb7a4145
Pasupuleti Venkatesh <pasupuletivenkatesh1900@gmail.com>
2022-05-02 23:34:42 +0530

    see

--- src/sumgit/multiply.java
+++ src/sumgit/multiply.java
@@ -5,7 +5,8 @@ public class multiply {
 	public static void main(String[] args) {
 		int a =5;
 		int b =10;
-		System.out.println("sum of : "+ a*b);
+		int c=100;
+		System.out.println("sum of : "+ a*b*c);
 
 	}
 
